16153f39a4807c5e96424dfc69d9d938813ee9e7
ananyabangera18 <116055065+ananyabangera18@users.noreply.github.com>
2024-02-15 23:16:24 +0530

    Create authors.js

---
+++ routes/authors.js
@@ -0,0 +1,10 @@
+const express =require('express')
+const authorController=require("../controllers/authorController")
+
+const router=express.Router()
+
+router.post('/login')
+router.post('/register',authorController.registerAuthor)
+
+module.exports =router
+
